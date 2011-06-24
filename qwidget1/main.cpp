328bba22b7c65a4a10a66c9cfa0b006235d31424
Masaharu Yajima <myajima0202@gmail.com>
2011-06-24 15:09:43 +0900

    modified

--- qwidget1/main.cpp
+++ qwidget1/main.cpp
@@ -1,18 +1,26 @@
 #include <QtGui>
+#include <QDial>
 
 int main(int argc, char *argv[])
 {
-  QApplication a(argc, argv);
+  QApplication app(argc, argv);
 
   QWidget *window = new QWidget;
 
   QDial *dial = new QDial;
-  dial->setRange(0,100);
+  dial->setRange(0,20);
+  dial->setNotchesVisible(true);
+  dial->setValue(10);
+
+  QPushButton *button = new QPushButton;
+  button->setText("Quit");
+  QObject::connect(button, SIGNAL(clicked()), &app, SLOT(quit()));
 
   QHBoxLayout *layout = new QHBoxLayout;
   layout->addWidget(dial);
+  layout->addWidget(button);
   window->setLayout(layout);
   window->show();
 
-  return a.exec();
+  return app.exec();
 }
